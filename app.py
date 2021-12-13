df9bc6e809e5af626ed729a2f04d50f880ba50ad
adityagum <63958188+adityagum@users.noreply.github.com>
2021-12-13 13:08:43 +0700

    Update app.py

--- app.py
+++ app.py
@@ -12,10 +12,29 @@ def index():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    features = [float(x) for x in request.form.values()]
+    X1, X2, X3, X4, X5, X6, X7, X8, X9, X10, X11, X12, X13, X14, X15, X16, X17, X18, X19, X20 = [float(x) for x in request.form.values()]
    
     data = []
-    data.append(features)
+    data.append(X1)
+    data.append(X2)
+    data.append(X3)
+    data.append(X4)
+    data.append(X5)
+    data.append(X6)
+    data.append(X7)
+    data.append(X8)
+    data.append(X9)
+    data.append(X10)
+    data.append(X11)
+    data.append(X12)
+    data.append(X13)
+    data.append(X14)
+    data.append(X15)
+    data.append(X16)
+    data.append(X17)
+    data.append(X18)
+    data.append(X19)
+    data.append(X20)
     prediction = model.predict(data)
 
     return render_template('index.html', result = prediction)
