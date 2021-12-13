5c767c37c33355d74ca8cbcf6fc30e25b64e6eab
adityagum <63958188+adityagum@users.noreply.github.com>
2021-12-13 12:12:05 +0700

    Update app.py

--- app.py
+++ app.py
@@ -16,25 +16,13 @@ def predict():
     Predict the insurance cost based on user inputs
     and render the result to the html page
     '''
-    age, sex, smoker = [x for x in request.form.values()]
-
+    features = [float(i) for i in request.form.values()]
+   
     data = []
+    data.append(features)
+    prediction = model.predict(data)
 
-    data.append(int(age))
-    if sex == 'Laki-laki':
-        data.extend([0, 1])
-    else:
-        data.extend([1, 0])
-
-    if smoker == 'Ya':
-        data.extend([0, 1])
-    else:
-        data.extend([1, 0])
-    
-    prediction = model.predict([data])
-    output = round(prediction[0], 2)
-
-    return render_template('index.html', insurance_cost=output, age=age, sex=sex, smoker=smoker)
+    return render_template('index.html', result = str(*prediction))
 
 
 if __name__ == '__main__':
